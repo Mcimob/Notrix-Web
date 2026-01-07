1a7020811e0b5ef74c456d44c1a7306fbf8a63f9
Mcimob <burckhardttim@gmail.com>
2026-01-07 17:20:09 +0100

    Changegs to MainLayout

--- src/main/java/ch/ethz/inf/peachlab/ui/MainLayout.java
+++ src/main/java/ch/ethz/inf/peachlab/ui/MainLayout.java
@@ -1,24 +1,17 @@
 package ch.ethz.inf.peachlab.ui;
 
 import ch.ethz.inf.peachlab.app.AppConfiguration;
-import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
-import com.vaadin.flow.component.applayout.DrawerToggle;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Footer;
-import com.vaadin.flow.component.html.Header;
-import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.Scroller;
-import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
 import com.vaadin.flow.spring.annotation.UIScope;
-import com.vaadin.flow.theme.lumo.LumoUtility;
 import org.springframework.stereotype.Component;
+
+import static ch.ethz.inf.peachlab.ui.DesignConstants.STYLE_FLEX_ALIGN_CENTER;
+import static ch.ethz.inf.peachlab.ui.DesignConstants.STYLE_FLEX_ROW;
+import static ch.ethz.inf.peachlab.ui.DesignConstants.STYLE_GAP_M;
+import static ch.ethz.inf.peachlab.ui.DesignConstants.STYLE_PADDING_M;
 
 /**
  * The main view is a top-level placeholder for other views.
@@ -31,54 +24,20 @@ public class MainLayout extends AppLayout {
 
     public MainLayout(AppConfiguration appConfiguration) {
         this.appConfiguration = appConfiguration;
-        setPrimarySection(Section.DRAWER);
-        addDrawerContent();
+        setPrimarySection(Section.NAVBAR);
         addHeaderContent();
     }
 
     private void addHeaderContent() {
-        DrawerToggle toggle = new DrawerToggle();
-        toggle.setAriaLabel("Menu toggle");
+        SideNavItem home = new SideNavItem(
+                getTranslation(appConfiguration.getApplicationTitle()),
+                "",
+                VaadinIcon.HOME.create());
 
-        HorizontalLayout leftLayout = new HorizontalLayout(toggle);
-        leftLayout.setAlignItems(FlexComponent.Alignment.BASELINE);
-        HorizontalLayout title = new HorizontalLayout(new Div(appConfiguration.getApplicationTitle()));
-        title.addClassName(DesignConstants.STYLE_FW_700);
-        title.setAlignItems(FlexComponent.Alignment.CENTER);
-
-        HorizontalLayout layout = new HorizontalLayout(leftLayout, title);
-        layout.setMargin(true);
-        layout.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
-        layout.setWidthFull();
+        Div layout = new Div(home);
+        layout.addClassNames(STYLE_FLEX_ROW, STYLE_FLEX_ALIGN_CENTER, STYLE_PADDING_M, STYLE_GAP_M);
 
         addToNavbar(true, layout);
-    }
-
-    private void addDrawerContent() {
-        Span appName = new Span(appConfiguration.getApplicationTitle());
-        appName.addClassNames(LumoUtility.FontWeight.SEMIBOLD, LumoUtility.FontSize.LARGE);
-        Header header = new Header(appName);
-
-        Scroller scroller = new Scroller(createNavigation());
-
-        addToDrawer(header, scroller, createFooter());
-    }
-
-    private SideNav createNavigation() {
-        SideNav nav = new SideNav();
-
-        nav.addItem(new SideNavItem(
-                getTranslation("view.home.title"),
-                "",
-                VaadinIcon.HOME.create()));
-
-        return nav;
-    }
-
-    private Footer createFooter() {
-        Footer layout = new Footer();
-
-        return layout;
     }
 
     @Override
