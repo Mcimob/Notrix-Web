50dc6bfdefd5891829dd1e6de014a580cb13c6d8
Mcimob <burckhardttim@gmail.com>
2026-01-07 16:59:11 +0100

    Added css

--- src/main/java/ch/ethz/inf/peachlab/ui/DesignConstants.java
+++ src/main/java/ch/ethz/inf/peachlab/ui/DesignConstants.java
@@ -2,18 +2,15 @@ package ch.ethz.inf.peachlab.ui;
 
 public final class DesignConstants {
 
-    private DesignConstants() { }
-
-    public static final String VIEW = "view";
-    public static final String VIEW_WIDE = "view-wide";
-
-    public static final String STYLE_CONTENT_MATCH_WIDTH = "content-match-width";
-    public static final String STYLE_WIDTH_NARROW = "width-narrow";
+    private DesignConstants() {}
 
     public static final String STYLE_FW_500 = "fw-500";
     public static final String STYLE_FW_700 = "fw-700";
 
+    public static final String STYLE_BACKGROUND_BG = "background-bg";
+
     public static final String STYLE_WIDTH_FULL = "width-full";
+    public static final String STYLE_HEIGHT_FULL = "height-full";
 
     public static final String STYLE_FLEX_COLUMN = "flex-column";
     public static final String STYLE_FLEX_ROW = "flex-row";
@@ -38,23 +35,10 @@ public final class DesignConstants {
     public static final String STYLE_BORDER_RADIUS_S = "border-radius-s";
     public static final String STYLE_BORDER_RADIUS_M = "border-radius-m";
     public static final String STYLE_BORDER_RADIUS_L = "border-radius-l";
-    public static final String STYLE_BORDER_RADIUS_ROUND = "border-radius-round";
 
     public static final String STYLE_FONT_SIZE_S = "font-size-s";
     //                         STYLE_FONT_SIZE_M is not needed, as it would just be regular font size
     public static final String STYLE_FONT_SIZE_L = "font-size-l";
 
-    public static final String STYLE_BG_LIGHT_REGULAR = "bg-light-regular";
-    public static final String STYLE_BG_REGULAR = "bg-regular";
-    public static final String STYLE_BG_DARK = "bg-dark";
-    public static final String STYLE_BG_LIGHT_ACCENT = "bg-light-accent";
-    public static final String STYLE_BG_TRANSITION = "bg-transition";
-
     public static final String STYLE_TEXT_COLOR_WHITE = "text-color-white";
-
-    public static final String CLR_ACCENT = "var(--clr-accent)";
-    public static final String CLR_BG = "var(--clr-bg)";
-    public static final String CLR_LIGHT = "var(--clr-light)";
-    public static final String CLR_REGULAR = "var(--clr-regular)";
-    public static final String CLR_DARK = "var(--clr-dark)";
 }
