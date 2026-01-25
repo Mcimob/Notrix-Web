cc27de0c087c199b8d787ed00cad13c3f41dcfe8
Mcimob <burckhardttim@gmail.com>
2026-01-25 16:44:22 +0100

    Addeed header to KernelView

--- src/main/java/ch/ethz/inf/peachlab/ui/views/kernel/KernelView.java
+++ src/main/java/ch/ethz/inf/peachlab/ui/views/kernel/KernelView.java
@@ -6,21 +6,31 @@ import ch.ethz.inf.peachlab.model.entity.KernelEntity;
 import ch.ethz.inf.peachlab.model.filter.KernelFilter;
 import ch.ethz.inf.peachlab.model.loadtype.KernelLoadType;
 import ch.ethz.inf.peachlab.ui.MainLayout;
+import ch.ethz.inf.peachlab.ui.components.ComponentWithLink;
 import ch.ethz.inf.peachlab.ui.components.TransitionSidebarReact;
 import ch.ethz.inf.peachlab.ui.views.AbstractView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.WildcardParameter;
 
 import static ch.ethz.inf.peachlab.ui.DesignConstants.STYLE_BACKGROUND_WHITE;
+import static ch.ethz.inf.peachlab.ui.DesignConstants.STYLE_FLEX_ALIGN_CENTER;
+import static ch.ethz.inf.peachlab.ui.DesignConstants.STYLE_FLEX_BETWEEN;
 import static ch.ethz.inf.peachlab.ui.DesignConstants.STYLE_FLEX_CENTER;
+import static ch.ethz.inf.peachlab.ui.DesignConstants.STYLE_FLEX_COLUMN;
 import static ch.ethz.inf.peachlab.ui.DesignConstants.STYLE_FLEX_ROW;
+import static ch.ethz.inf.peachlab.ui.DesignConstants.STYLE_GAP_M;
+import static ch.ethz.inf.peachlab.ui.DesignConstants.STYLE_GAP_S;
 import static ch.ethz.inf.peachlab.ui.DesignConstants.STYLE_HEIGHT_FULL;
+import static ch.ethz.inf.peachlab.ui.DesignConstants.STYLE_PADDING_S;
 import static ch.ethz.inf.peachlab.ui.DesignConstants.STYLE_WIDTH_FULL;
 
 @Route(value = "code", layout = MainLayout.class)
@@ -43,8 +53,8 @@ public class KernelView extends AbstractView implements HasUrlParameter<String> 
     @Override
     public void render() {
         removeAll();
-        Div center = new Div();
-        center.addClassNames(STYLE_HEIGHT_FULL, STYLE_WIDTH_FULL, STYLE_BACKGROUND_WHITE);
+        Div center = new Div(createHeader());
+        center.addClassNames(STYLE_HEIGHT_FULL, STYLE_WIDTH_FULL, STYLE_FLEX_COLUMN, STYLE_GAP_M);
 
         Div right = new Div();
         right.addClassNames(STYLE_HEIGHT_FULL, STYLE_BACKGROUND_WHITE);
@@ -61,6 +71,33 @@ public class KernelView extends AbstractView implements HasUrlParameter<String> 
         Div div = new Div(sidebar);
         div.addClassNames(STYLE_FLEX_ROW, STYLE_FLEX_CENTER, STYLE_BACKGROUND_WHITE);
         div.setWidth("50%");
+
+        return div;
+    }
+
+    private Component createHeader() {
+        Div textDiv = new Div();
+        textDiv.addClassNames(STYLE_FLEX_COLUMN, STYLE_GAP_S);
+        textDiv.add(new ComponentWithLink(
+            new H2(kernel.getTitle()),
+            "https://kaggle.com/code/%s/%s".formatted(kernel.getAuthorUserName(), kernel.getCurrentUrlSlug())
+        ));
+        textDiv.add(new ComponentWithLink(
+            new Text("By " + kernel.getAuthorDisplayName()),
+                "https://kaggle.com/%s".formatted(kernel.getAuthorUserName())
+        ));
+
+        Div iconsDiv = new Div();
+        iconsDiv.addClassNames(STYLE_FLEX_ROW, STYLE_GAP_S);
+        Icon download = VaadinIcon.DOWNLOAD.create();
+        download.setSize("32px");
+        Icon bookmark = VaadinIcon.BOOKMARK_O.create();
+        bookmark.setSize("32px");
+        iconsDiv.add(download, bookmark);
+
+        Div div = new Div(textDiv, iconsDiv);
+        div.addClassNames(STYLE_FLEX_ROW, STYLE_FLEX_BETWEEN, STYLE_FLEX_ALIGN_CENTER,
+            STYLE_WIDTH_FULL, STYLE_BACKGROUND_WHITE, STYLE_PADDING_S);
 
         return div;
     }
