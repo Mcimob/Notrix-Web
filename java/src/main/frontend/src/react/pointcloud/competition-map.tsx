ece3bdfd3cbefd2a99b30fd8066b1c58d0fd419e
Mcimob <burckhardttim@gmail.com>
2026-03-17 16:44:40 +0100

    Code cleanup

--- java/src/main/frontend/src/react/pointcloud/competition-map.tsx
+++ java/src/main/frontend/src/react/pointcloud/competition-map.tsx
@@ -1,6 +1,5 @@
-import React, { useRef, useEffect } from "react";
+import React, {useEffect, useRef} from "react";
 import * as d3 from "d3";
-import {schemePaired} from "d3-scale-chromatic";
 
 export type Competition = {
     id: string;
@@ -71,6 +70,7 @@ const CompetitionMap: React.FC<Props> = (
         // -----------------------------
         // Compute scales
         // -----------------------------
+        // Main Canvas scales
         const xExtent = d3.extent(competitions, d => d.coordinateX) as [number, number];
         const yExtent = d3.extent(competitions, d => d.coordinateY) as [number, number];
 
@@ -84,109 +84,12 @@ const CompetitionMap: React.FC<Props> = (
             .domain(yExtent)
             .range([height - 50, 50]);
 
-        const quadtree = d3.quadtree<Competition>()
-            .x(d => xScale(d.coordinateX))
-            .y(d => yScale(d.coordinateY))
-            .addAll(competitions);
-
-        // -----------------------------
-        // Draw clusters
-        // -----------------------------
-
-        const scaleEllipseX = (c: Cluster, r: number) => xScale(c.centroidX + r) - xScale(c.centroidX);
-        const scaleEllipseY = (c: Cluster, r: number) => yScale(c.centroidY) - yScale(c.centroidY + r);
-
-        const opacityLevels = [0.3, 0.15, 0.07]; // inner to outer
-
-        g.selectAll("g.cluster")
-            .data(clusters)
-            .enter()
-            .append("g")
-            .attr("class", "cluster")
-            .each(function(c) {
-                const gCluster = d3.select(this);
-
-                // draw ellipses: mean radius, mean ± 1 std, mean ± 2 std
-                const radii = [
-                    [c.radiusX, c.radiusY],               // mean
-                    [c.radiusX + c.stdX, c.radiusY + c.stdY],
-                    [c.radiusX + 2*c.stdX, c.radiusY + 2*c.stdY]
-                ];
-
-                radii.forEach((r, i) => {
-                    gCluster.append("ellipse")
-                        .attr("cx", xScale(c.centroidX))
-                        .attr("cy", yScale(c.centroidY))
-                        .attr("rx", scaleEllipseX(c, r[0]))
-                        .attr("ry", scaleEllipseY(c, r[1]))
-                        .attr("fill", d3.hsl(c.id * 360 / clusters.length, 0.5, 0.6).toString())
-                        .attr("opacity", opacityLevels[i])
-                        .style("pointer-events", "none");
-                });
-            });
-
-        // -----------------------------
-        // Draw points
-        // -----------------------------
-        const maxCircleSize = 10;
-        const minCircleSize = 3;
-        g.selectAll("circle")
-            .data(competitions)
-            .enter()
-            .append("circle")
-            .attr("cx", d => xScale(d.coordinateX))
-            .attr("cy", d => yScale(d.coordinateY))
-            .attr("r", d => minCircleSize * Math.pow((maxCircleSize / minCircleSize), d.totalSubmissions / maxSubmissions))
-            .attr("fill", d => d3.hsl(d.clusterId * 360 / clusters.length, 0.5, 0.6).toString())
-            .attr("opacity", 0.8)
-            .on("click", (_, d) => clickListener(d.id))
-            .append("title")
-            .text(d => d.title)
-
-        const highlight = g.append("circle")
-            .attr("r", maxCircleSize)
-            .attr("fill", "none")
-            .attr("stroke", "orange")
-            .attr("stroke-width", 2)
-            .style("pointer-events", "none")
-            .style("opacity", 0);
-
-    g.selectAll("text")
-        .data(clusters)
-        .enter()
-        .append("text")
-        .attr("x", c => xScale(c.centroidX))
-        .attr("y", c => yScale(c.centroidY))
-        .attr("text-anchor", "middle")
-        .attr("font-size", "18px")
-        .attr("font-weight", "bold")
-        .attr("opacity", 0.8)
-        .text(c => c.description);
-
+        // Minimap Scales
         const minimapWidth = 200;
         const minimapHeight = 150;
 
-        const minimap = svg.append("g")
-            .attr("transform", `translate(${width - minimapWidth - 10}, ${height - minimapHeight - 10})`)
-            .style("filter", "drop-shadow(0px 2px 4px rgba(0,0,0,0.2))")
-            .attr("opacity", 0.8);
-
-        minimap.append("rect")
-            .attr("width", minimapWidth)
-            .attr("height", minimapHeight)
-            .attr("fill", "#fff")
-            .attr("stroke", "#ccc");
-
-        const miniContent = minimap.append(() =>
-            base.node()!.cloneNode(true) as SVGGElement
-        );
-        miniContent.selectAll("*").style("pointer-events", "none");
-        miniContent.selectAll("text").remove();
-
         const scaleX = minimapWidth / width;
         const scaleY = minimapHeight / height;
-
-        miniContent.attr("transform", `scale(${scaleX}, ${scaleY})`);
 
         const miniX = d3.scaleLinear()
             .domain(xExtent)
@@ -196,19 +99,177 @@ const CompetitionMap: React.FC<Props> = (
             .domain(yExtent)
             .range([minimapHeight, 0]);
 
-        const viewport = minimap.append("rect")
-            .attr("fill", "var(--lumo-primary-text-color)")
-            .attr("opacity", "0.2")
-            .attr("stroke", "var(--lumo-primary-text-color)")
-            .attr("stroke-width", 1.5)
+        // Quadtree for efficient competition searching
+        const quadtree = d3.quadtree<Competition>()
+            .x(d => xScale(d.coordinateX))
+            .y(d => yScale(d.coordinateY))
+            .addAll(competitions);
 
-        svg.append("defs")
-            .append("clipPath")
-            .attr("id", "minimap-clip")
-            .append("rect")
-            .attr("width", minimapWidth)
-            .attr("height", minimapHeight);
-        minimap.attr("clip-path", "url(#minimap-clip)");
+        const drawClusters = () => {
+
+            const scaleEllipseX = (c: Cluster, r: number) => xScale(c.centroidX + r) - xScale(c.centroidX);
+            const scaleEllipseY = (c: Cluster, r: number) => yScale(c.centroidY) - yScale(c.centroidY + r);
+
+            const opacityLevels = [0.3, 0.15, 0.07]; // inner to outer
+
+            g.selectAll("g.cluster")
+                .data(clusters)
+                .enter()
+                .append("g")
+                .attr("class", "cluster")
+                .each(function(c) {
+                    const gCluster = d3.select(this);
+
+                    // draw ellipses: mean radius, mean ± 1 std, mean ± 2 std
+                    const radii = [
+                        [c.radiusX, c.radiusY],               // mean
+                        [c.radiusX + c.stdX, c.radiusY + c.stdY],
+                        [c.radiusX + 2*c.stdX, c.radiusY + 2*c.stdY]
+                    ];
+
+                    radii.forEach((r, i) => {
+                        gCluster.append("ellipse")
+                            .attr("cx", xScale(c.centroidX))
+                            .attr("cy", yScale(c.centroidY))
+                            .attr("rx", scaleEllipseX(c, r[0]))
+                            .attr("ry", scaleEllipseY(c, r[1]))
+                            .attr("fill", d3.hsl(c.id * 360 / clusters.length, 0.5, 0.6).toString())
+                            .attr("opacity", opacityLevels[i])
+                            .style("pointer-events", "none");
+                    });
+                });
+        }
+
+        const drawPoints = () => {
+            // -----------------------------
+            // Draw points
+            // -----------------------------
+            const maxCircleSize = 10;
+            const minCircleSize = 3;
+            g.selectAll("circle")
+                .data(competitions)
+                .enter()
+                .append("circle")
+                .attr("cx", d => xScale(d.coordinateX))
+                .attr("cy", d => yScale(d.coordinateY))
+                .attr("r", d => minCircleSize * Math.pow((maxCircleSize / minCircleSize), d.totalSubmissions / maxSubmissions))
+                .attr("fill", d => d3.hsl(d.clusterId * 360 / clusters.length, 0.5, 0.6).toString())
+                .attr("opacity", 0.8)
+                .on("click", (_, d) => clickListener(d.id))
+                .append("title")
+                .text(d => d.title)
+
+            const highlight = g.append("circle")
+                .attr("r", maxCircleSize)
+                .attr("fill", "none")
+                .attr("stroke", "orange")
+                .attr("stroke-width", 2)
+                .style("pointer-events", "none")
+                .style("opacity", 0);
+
+
+            let lastClosest : Competition;
+            svg.on("mousemove", (event) => {
+
+                const [mx, my] = d3.pointer(event);
+
+                const tx = currentTransform.invertX(mx);
+                const ty = currentTransform.invertY(my);
+
+                const closest = quadtree.find(tx, ty, 40);
+
+                if (closest) {
+                    if (lastClosest != closest) {
+                        closestChangeListener(closest.id)
+                        lastClosest = closest;
+                        titleText.text(closest.title);
+
+                        highlight
+                            .attr("cx", xScale(closest.coordinateX))
+                            .attr("cy", yScale(closest.coordinateY))
+                            .style("opacity", 1);
+                    }
+                } else {
+                    highlight.style("opacity", 0);
+                }
+            });
+        }
+
+        const drawMinimap = () => {
+            const minimap = svg.append("g")
+                .attr("transform", `translate(${width - minimapWidth - 10}, ${height - minimapHeight - 10})`)
+                .style("filter", "drop-shadow(0px 2px 4px rgba(0,0,0,0.2))")
+                .attr("opacity", 0.8);
+
+            minimap.append("rect")
+                .attr("width", minimapWidth)
+                .attr("height", minimapHeight)
+                .attr("fill", "#fff")
+                .attr("stroke", "#ccc");
+
+            const miniContent = minimap.append(() =>
+                base.node()!.cloneNode(true) as SVGGElement
+            );
+            miniContent.selectAll("*").style("pointer-events", "none");
+            miniContent.selectAll("text").remove();
+
+            miniContent.attr("transform", `scale(${scaleX}, ${scaleY})`);
+
+            const viewport = minimap.append("rect")
+                .attr("fill", "var(--lumo-primary-text-color)")
+                .attr("opacity", "0.2")
+                .attr("stroke", "var(--lumo-primary-text-color)")
+                .attr("stroke-width", 1.5)
+
+            svg.append("defs")
+                .append("clipPath")
+                .attr("id", "minimap-clip")
+                .append("rect")
+                .attr("width", minimapWidth)
+                .attr("height", minimapHeight);
+            minimap.attr("clip-path", "url(#minimap-clip)");
+
+            minimap.on("click", (event) => {
+                const [mx, my] = d3.pointer(event);
+
+                const targetX = miniX.invert(mx);
+                const targetY = miniY.invert(my);
+
+                const scale = currentTransform.k;
+
+                const newTransform = d3.zoomIdentity
+                    .translate(
+                        width / 2 - xScale(targetX) * scale,
+                        height / 2 - yScale(targetY) * scale
+                    )
+                    .scale(scale);
+
+                svg.transition()
+                    .duration(500)
+                    .call(zoom.transform, newTransform);
+            });
+
+            return viewport;
+        }
+
+        const drawClusterTexts = () => {
+            g.selectAll("text")
+                .data(clusters)
+                .enter()
+                .append("text")
+                .attr("x", c => xScale(c.centroidX))
+                .attr("y", c => yScale(c.centroidY))
+                .attr("text-anchor", "middle")
+                .attr("font-size", "18px")
+                .attr("font-weight", "bold")
+                .attr("opacity", 0.8)
+                .text(c => c.description);
+        }
+
+        drawClusters();
+        drawPoints();
+        drawClusterTexts();
+        const viewport = drawMinimap();
 
         // -----------------------------
         // Zoom behavior
@@ -243,52 +304,6 @@ const CompetitionMap: React.FC<Props> = (
 
         svg.call(zoom);
         svg.call(zoom.transform, d3.zoomIdentity);
-
-        let lastClosest : Competition;
-        svg.on("mousemove", (event) => {
-
-            const [mx, my] = d3.pointer(event);
-
-            const tx = currentTransform.invertX(mx);
-            const ty = currentTransform.invertY(my);
-
-            const closest = quadtree.find(tx, ty, 40);
-
-            if (closest) {
-                if (lastClosest != closest) {
-                    closestChangeListener(closest.id)
-                    lastClosest = closest;
-                    titleText.text(closest.title);
-
-                    highlight
-                        .attr("cx", xScale(closest.coordinateX))
-                        .attr("cy", yScale(closest.coordinateY))
-                        .style("opacity", 1);
-                }
-            } else {
-                highlight.style("opacity", 0);
-            }
-        });
-
-        minimap.on("click", (event) => {
-            const [mx, my] = d3.pointer(event);
-
-            const targetX = miniX.invert(mx);
-            const targetY = miniY.invert(my);
-
-            const scale = currentTransform.k;
-
-            const newTransform = d3.zoomIdentity
-                .translate(
-                    width / 2 - xScale(targetX) * scale,
-                    height / 2 - yScale(targetY) * scale
-                )
-                .scale(scale);
-
-            svg.transition()
-                .duration(500)
-                .call(zoom.transform, newTransform);
-        });
 
     }, [competitions, clusters, width, height]);
 
